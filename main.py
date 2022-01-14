5d6a5dcd064fa4bde812c27bcec62e4a4edde68c
feloeche <feloeche@gmail.com>
2022-01-14 14:39:12 -0500

    models

--- main.py
+++ main.py
@@ -1,6 +1,47 @@
+#Python
+from uuid import UUID
+from datetime import date
+from typing import Optional
+
+# Pydantic
+from pydantic import BaseModel
+
+# FastAPI
 from fastapi import FastAPI
+from pydantic.networks import EmailStr
+from pydantic import Field
+
 
 app = FastAPI()
+
+# Models
+
+class UserBase(BaseModel):
+    user_id: UUID = Field(...)
+    email: EmailStr = Field(...)
+
+class UserLogin(UserBase):
+        password: str = Field(
+        ...,
+        min_length=8,
+        max_length=64
+    )
+
+class User(UserBase):
+    first_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50,
+        )
+    first_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50,
+        )
+    birth_date: Optional[date] = Field(default=None)
+
+class TWeet(BaseModel):
+    pass
 
 @app.get(path="/")
 def home():

75eaa35cca41730e195eaf162ce96fa0ea7785bf
feloeche <feloeche@gmail.com>
2022-01-17 14:46:26 -0500

    userse_signup

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #Python
+import json
 from uuid import UUID
 from datetime import date, datetime
 from typing import Optional, List
@@ -11,6 +12,7 @@ from pydantic.networks import EmailStr
 # FastAPI
 from fastapi import FastAPI
 from fastapi import status
+from fastapi import Body
 
 
 app = FastAPI()
@@ -41,6 +43,13 @@ class User(UserBase):
         )
     birth_date: Optional[date] = Field(default=None)
 
+class UserRegister(User):
+    password: str = Field(
+        ...,
+        min_length=8,
+        max_length=64
+    )
+
 class Tweet(BaseModel):
     tweet_id: UUID = Field(...)
     content: str = Field(
@@ -65,8 +74,34 @@ class Tweet(BaseModel):
     summary="Register a user",
     tags=["Users"]
     )
-def signup():
-    pass
+def signup(user: UserRegister = Body(...)):
+    """
+    Sign up
+
+    This path operations register a user in the app
+
+    Paremeters:
+        -Request body parameter
+            -user: UserRegister
+
+    Returns a json with the basic user information:
+        - user_id: UUID
+        - email: Emailstr
+        - first_name: str
+        - last_name: str
+        - birth_date: datetime 
+    """
+    with open("users.json", "r+", encoding="utf-8") as f:
+        results = json.loads(f.read())
+        user_dict = user.dict()
+        user_dict["user_id"] = str(user_dict["user_id"])
+        user_dict["birth_date"] = str(user_dict["birth_date"])
+        results.append(user_dict)
+        f.seek(0)
+        f.write(json.dumps(results))
+        return user
+
+
 
 ### Login a user
 @app.post(
